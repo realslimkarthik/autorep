350fec22ecf1295280838047f5838c4b8cacfd3e
karthikhariharan <karthikhariharan13@gmail.com>
2014-10-30 21:30:03 -0500

    Batch Process updated

--- batch/batchProcess.py
+++ batch/batchProcess.py
@@ -1,34 +1,38 @@
-import random
 import MySQLdb as mdb
 import sys
+import requests
+import json
+#threshold = 100
 
-try:
-    con = mdb.connect('localhost', 'root', '', 'autorep')
+def db_check():
+    notifyJSON = {}
+    try:
+        con = mdb.connect('localhost', 'root', '', 'autorep')
+        cur = con.cursor()
+        cur.execute("SELECT ID FROM STORE")
+        stores = cur.fetchall()
+        cur.execute("SELECT ID, MINSHELFQUANTITY FROM PRODUCT")
+        productList = cur.fetchall()
+        for i in stores:
+            for j in productList:
+                cur.execute("SELECT COUNT(" + j[0] + ") FROM SKU WHERE STORE_ID=" + i[0])
+                pIDCount = cur.fetchall()
+                if pIDCount[0] < j[1]:
+                    notifyJSON[i].append(j[0])
 
-    cur = con.cursor()
-    cur.execute("SELECT ID FROM STORE")
-    stores = cur.fetchall()
-    cur.execute("SELECT DISTINCT(ID) FROM PRODUCT")
-    productList = cur.fetchall()
-    for i in stores:
-        for j in productList:
-            cur.execute("SELECT COUNT(" + j[0] + ") FROM SKU WHERE ID=" + i[0])
-            pIDCount = cur.fetchall()
-            if pIDCount[0] < threshold:
-                notifyJSON[i].append(j)
+    except mdb.Error, e:
+        print "Error %d: %s" % (e.args[0], e.args[1])
+        sys.exit(1)
 
-    cur.execute("SHOW TABLES")
-    result = cur.fetchall()
-    for i in result:
-        print i[0]
+    finally:
+        if con:
+            con.close()
 
-except mdb.Error, e:
 
-    print "Error %d: %s" % (e.args[0], e.args[1])
-    sys.exit(1)
+def notifyBackend(resultJSON):
+    payload = {'result': resultJSON}
+    r = requests.post("http://54.172.105.21/webservices/notify/", params=payload)
 
-finally:
-
-    if con:
-        con.close()
-
+if __name__ == "__main__":
+    resultJSON = db_check()
+    notifyBackend(resultJSON)
