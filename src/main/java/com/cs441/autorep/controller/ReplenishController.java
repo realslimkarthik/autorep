618472fa3c45773f200cd2b9971c6e4a0438b688
Anirudh Ramnath <anirudh.ramnath@live.com>
2014-11-29 16:20:38 -0600

    comments

--- src/main/java/com/cs441/autorep/controller/ReplenishController.java
+++ src/main/java/com/cs441/autorep/controller/ReplenishController.java
@@ -32,6 +32,13 @@ public class ReplenishController {
 	@Autowired
 	RepSuggestionsManager repsuggestionsManager;
 	
+	/**
+	 * Redirects the user to `replenish.jsp`
+	 * @param req
+	 * @param res
+	 * @return
+	 * @throws Exception
+	 */
 	@RequestMapping(value = "/replenish", method = RequestMethod.GET)
 	public ModelAndView showDashboard(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		logger.info("Welcome to replenish");
@@ -52,7 +59,13 @@ public class ReplenishController {
 		return model;
 	}
 	
-	
+	/**
+	 * Shows replenishment page according to the selected `Store ID`
+	 * @param req
+	 * @param res
+	 * @return ModelAndView (redirects the user to `replenish.jsp`)
+	 * @throws Exception
+	 */
 	@RequestMapping(value = "/showReplenish", method = RequestMethod.POST)
 	public ModelAndView showInventory(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		logger.info("Inside showReplenish method => ReplenishController");
@@ -72,6 +85,14 @@ public class ReplenishController {
 		return model;
 	}
 	
+	/**
+	 * This method is called when a user approves replenishment suggestions. Product details are moved from ReplenishmentSuggestions
+	 * to SKU table.
+	 * @param req
+	 * @param res
+	 * @return
+	 * @throws Exception
+	 */
 	@RequestMapping(value = "/applyReplenish", method = RequestMethod.POST)
 	public ModelAndView replenished(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		logger.info("Inside replenished method => ReplenishController");
@@ -89,7 +110,12 @@ public class ReplenishController {
 		
 	}
 	
-	
+	/**
+	 * Gets the list of replenishment suggestions from the database.
+	 * @param storeId
+	 * @return
+	 * @throws Exception
+	 */
 	private ArrayList<RepSuggestions> getReplenishList(String storeId) throws Exception{
 		
 		ArrayList<RepSuggestions> repsuggestionsList = repsuggestionsManager.getReplenish(storeId);
